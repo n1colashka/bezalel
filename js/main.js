d552da4422508c07bab46a9c619e5a84992380bc
Nick <deckards2505@gmail.com>
2020-11-16 17:11:42 +0600

    feat: add header

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 $(function(){
 
     
+    function initMenu() {
+        $('.header__menu-btn').on('click', () => {
+            $('.header__menu').slideDown(500);
+            $('html').addClass('block-scroll');
+        });
+        $('.header__menu-close').on('click', () => {
+            $('.header__menu').slideUp(500);
+            $('html').removeClass('block-scroll');
+        });
+    }
+    
     
     if (window.innerWidth <= 768) {
-        
+        initMenu();
     }
     
 });
